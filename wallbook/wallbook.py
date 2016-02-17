ab892e7193ef20e9b6a72d6901bdb7945cd5f5cb
jnaviello <jason.naviello@neulion.com>
2016-02-17 10:53:31 -0800

    update
    updates

--- wallbook/wallbook.py
+++ wallbook/wallbook.py
@@ -1,12 +1,12 @@
-import cgi
-import urllib
 import jinja2
+import webapp2
+import urllib
 import os
-
+import time
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-import webapp2
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -14,109 +14,81 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), aut
 DEFAULT_WALL = 'Public'
 
 
-def wall_key(wall_name=DEFAULT_WALL):
-    """Constructs a Datastore key for a comment entity.
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-    We use comment_name as the key.
-    """
-    return ndb.Key('Wall', wall_name)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def guestbook_key(guestbook_name=DEFAULT_WALL):
+    return ndb.Key('Guestbook', guestbook_name)
 
 class Author(ndb.Model):
-    """Sub model for representing an author."""
-    identity = ndb.StringProperty(indexed=True)
-    name = ndb.StringProperty(indexed=False)
+    identity = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
 
-
-class Post(ndb.Model):
-    """A main model for representing an individual post entry."""
-    author = ndb.StructuredProperty(Author)
-    content = ndb.StringProperty(indexed=False)
+class Greeting(ndb.Model):
+    name = ndb.StringProperty()
+    comment = ndb.StringProperty(indexed=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 
-class MainPage(webapp2.RequestHandler):
+
+class MainPage(Handler):
     def get(self):
-        comment_name = self.request.get('wall_name', DEFAULT_WALL)
-        if comment_name == DEFAULT_WALL.lower(): comment_name = DEFAULT_WALL
-
+        guestbook_name = self.request.get('guestbook_name', DEFAULT_WALL)
+        max_posts=20
+        error= self.request.get('error', "")
         # [START query]
-        posts_query = Post.query(ancestor=wall_key(wall_name)).order(-Post.date)
-
-        posts = posts_query.fetch()
-
-        # If a person is logged into Google's Services
+        greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(max_posts)
+        # Check if a person is logged into Google's Services
         user = users.get_current_user()
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
-            user_name = user.nickname()
+
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-            user_name = 'Anonymous Poster'
-
-        # Create our posts html
-        posts_html = ''
-        for post in posts:
-
-            # Check if the current signed in user matches with the author's identity from this particular
-            # post. Newline character '\n' tells the computer to print a newline when the browser is
-            # is rendering our HTML
-            if user and user.user_id() == post.author.identity:
-                posts_html += '<div><h3>(You) ' + post.author.name + '</h3>\n'
-            else:
-                posts_html += '<div><h3>' + post.author.name + '</h3>\n'
-
-            posts_html += 'wrote: <blockquote>' + cgi.escape(post.content) + '</blockquote>\n'
-            posts_html += '</div>\n'
-
-        sign_query_params = urllib.urlencode({'wall_name': wall_name})
-
-        # Render our page
-        self.render("comments.html", sign_query_params, cgi.escape(comment_name),
-                                         url, url_linktext)
-
-        # Write Out Page here
-        self.response.out.write(rendered_HTML)
 
 
-class PostWall(webapp2.RequestHandler):
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+            'error': error
+        }
+
+        template = jinja_env.get_template('Notes.html')
+        self.response.write(template.render(template_values))
+
+
+class Guestbook(Handler):
     def post(self):
-        # We set the same parent key on the 'Post' to ensure each
-        # Post is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        wall_name = self.request.get('wall_name', DEFAULT_COMMENT_NAME)
-        post = Post(parent=comment_key(wall_name))
-
-        # When the person is making the post, check to see whether the person
-        # is logged into Google
+        guestbook_name = self.request.get('guestbook_name', DEFAULT_WALL).strip()
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
         if users.get_current_user():
-            post.author = Author(
-                identity=users.get_current_user().user_id(),
-                name=users.get_current_user().nickname(),
-                email=users.get_current_user().email())
+            greeting.author = Author(identity=users.get_current_users().user_id(),email=users.get_current_user().email)
+        greeting.content = self.request.get('content')
+        greeting.content.strip()
+        if greeting.content == '':
+            error = "I'm sorry that's not a valid comment!"
+            self.redirect('/?' + error)
         else:
-            post.author = Author(
-                name='anonymous@anonymous.com',
-                email='anonymous@anonymous.com')
-
-
-        # Get the content from our request parameters, in this case, the message
-        # is in the parameter 'content'
-        post.content = self.request.get('content')
-
-        # Write to the Google Database
-        post.put()
-
-        # Do other things here such as a page redirect
-        self.redirect('/?wall_name=' + wall_name)
+            greeting.put()
+            query_params = {'guestbook_name':guestbook_name}
+            self.redirect('/?'+ urllib.urlencode(query_params))
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', PostWall),
-], debug=True)
+    ('/sign', Guestbook),
+    ], debug=True)
